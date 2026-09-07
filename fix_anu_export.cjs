654540e01e7d24a5179b15bf19b5699045ea7115
Samuel Victor <samuelvi3tor@gmail.com>
2026-09-07 22:49:15 +0530

    chore: clean up legacy tracked scripts from root

--- fix_anu_export.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let indexTs = fs.readFileSync('src/utils/mappings/index.ts', 'utf8');
-if (indexTs.includes('ANU6_UNICODE_TO_NONUNICODE')) {
-    console.log("Export is correct.");
-}
--- test_char.js
+++
@@ -1,5 +0,0 @@
-const code = 0xF0FE;
-const signedUni = code > 32767 ? code - 65536 : code;
-const winCode = code - 0xF000;
-console.log(`\\u${signedUni}\\'${winCode.toString(16).padStart(2, '0')}`);
-// \u-3842\'fe
