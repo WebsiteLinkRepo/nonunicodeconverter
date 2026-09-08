dd5c884d33e37fa3af2e09f6cfd26d2b9868682c
Samuel Victor <samuelvi3tor@gmail.com>
2026-09-08 10:10:15 +0530

    commit before pg

---
+++ test-font-mapping.js
@@ -0,0 +1,10 @@
+const mappings = {
+  telugu: { unicodeFont: 'Mandali, Gautami, "Noto Sans Telugu", sans-serif' },
+  hindi: { unicodeFont: 'Mangal, "Noto Sans Devanagari", sans-serif' },
+  tamil: { unicodeFont: 'Latha, "Noto Sans Tamil", sans-serif' },
+  kannada: { unicodeFont: 'Tunga, "Noto Sans Kannada", sans-serif' },
+  malayalam: { unicodeFont: 'Kartika, "Noto Sans Malayalam", sans-serif' },
+  marathi: { unicodeFont: 'Mangal, "Noto Sans Devanagari", sans-serif' },
+  gujarati: { unicodeFont: 'Shruti, "Noto Sans Gujarati", sans-serif' }
+};
+console.log(mappings);
